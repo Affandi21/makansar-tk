e1bd0881e21235166c228f1d43283fd221271e2b
mansizsultan <sultanibnumansiz@gmail.com>
2024-10-10 23:39:40 +0700

    Menambahkan fungsi show_main semenyata

--- main/views.py
+++ main/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def show_main(request):
+    context = {
+        'team' : 'D04',
+    }
+
+    return render(request, "main.html", context)
+
+
