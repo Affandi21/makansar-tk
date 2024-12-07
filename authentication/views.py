2338910b27f70596a9aeb228edac7c2939fcb413
Stefanus Tan Jaya <stefanustanjaya230105@gmail.com>
2024-12-07 22:08:18 +0700

    feat: Add logout auth

--- authentication/views.py
+++ authentication/views.py
@@ -76,3 +76,20 @@ def register(request):
             "status": False,
             "message": "Invalid request method."
         }, status=400)
+    
+@csrf_exempt
+def logout(request):
+    username = request.user.username
+
+    try:
+        auth_logout(request)
+        return JsonResponse({
+            "username": username,
+            "status": True,
+            "message": "Logout berhasil!"
+        }, status=200)
+    except:
+        return JsonResponse({
+        "status": False,
+        "message": "Logout gagal."
+        }, status=401)
