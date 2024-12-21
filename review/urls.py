34930254f86abb933692b95c79470a13bab9b3a9
gilbertkristiaan <gilbertkristian79@gmail.com>
2024-12-21 20:51:07 +0700

    progress backend django review selesai

--- review/urls.py
+++ review/urls.py
@@ -6,8 +6,10 @@ app_name = 'review'
 urlpatterns = [
     path('makanan/<int:makanan_id>/reviews/', views.show_reviews, name='show_reviews'),
     path('makanan/<int:makanan_id>/tambah_review/', views.tambah_review, name='tambah_review'),
-    path('review/<int:review_id>/edit/', views.edit_review, name='edit_review'),
-    path('review/<int:review_id>/hapus/', views.hapus_review, name='hapus_review'),
+    path('<int:review_id>/edit/', views.edit_review, name='edit_review'),
+    path('<int:review_id>/hapus/', views.hapus_review, name='hapus_review'),
     path('makanan/<int:makanan_id>/reviews/json/', views.show_json_reviews, name='show_json_reviews'),
     path('makanan/<int:pk>/create-review-flutter/', views.create_review_flutter, name='create_review_flutter'),
+    path('current_username/', views.get_current_username, name='get_current_username'),
+
 ]
